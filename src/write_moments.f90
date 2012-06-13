c26a2537674452e58f30119539d93a9109dddab1
Brian Friesen <friesen@ou.edu>
2012-06-13 17:09:32 -0500

    added Planck function to write_moments so one can see that J -> B at depth

--- src/write_moments.f90
+++ src/write_moments.f90
@@ -1,14 +1,15 @@
 subroutine write_moments
   use global
+  use interfaces, only: planck_fn
   implicit none
 
   integer :: i1, i2
-  character(len=*), parameter :: wfmt = '(4es12.4e2)'
+  character(len=*), parameter :: wfmt = '(5es12.4e2)'
   
   do i1 = 1, n_wl_pts
     do i2 = 1, n_depth_pts
       write( 23, wfmt ) tau_grid( i2 ), j_lambda( i2, i1 ), h_lambda( i2, i1 ), &
-      k_lambda( i2, i1 )
+      k_lambda( i2, i1 ), planck_fn( wl_grid( i1 ), temp )
     end do
   end do
 
