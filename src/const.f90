bd7bb4869b4e0727f4cbcae191dc6b42b6b5ab5a
Brian Friesen <friesen@ou.edu>
2012-12-20 09:52:16 -0600

    Added more physical constants to const module

--- src/const.f90
+++ src/const.f90
@@ -2,6 +2,8 @@ module const
   use precision_mod
   implicit none
 
-  real(kind=dp), parameter :: pi = 3.14159265359d+0
+  real(kind=dp), parameter :: pi       = 3.14159265359d0, &
+                              h_planck = 6.626d-27, &
+                              k_boltz  = 1.381d-16
 
 end module const
