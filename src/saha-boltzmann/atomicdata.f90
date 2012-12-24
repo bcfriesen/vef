b5c27003c5fcbd1d3c17feb880fd806d8cf52eab
Brian Friesen <friesen@ou.edu>
2012-12-24 08:53:24 -0600

    Cleaned up Doxygen documentation a little bit.

--- src/saha-boltzmann/atomicdata.f90
+++ src/saha-boltzmann/atomicdata.f90
@@ -6,12 +6,12 @@ MODULE atomicdata
 
   INTEGER, PARAMETER :: nlvlmax = 100 !< maximum number of levels for any given atom.
 
-  INTEGER :: nlvl(92, 0:92) !< nlvl\f$(i,j)\f$: actual number of levels to consider for ion \f$j\f$ of
-                            !! atom \f$i\f$. nlvl\f$(i,j)=0\f$ if \f$j>i\f$. \f$n\f$ is equal to the
-                            !! index \f$k\f$ in the formalism from class. Just for future reference,
-                            !! \f$i=1 \rightarrow\f$ hydrogen, \f$j=0 \rightarrow\f$ neutral,
-                            !! \f$k=1 \rightarrow\f$ ground state.
-                            !! hence the non-standard indices for \f$j\f$ in a whole bunch of these
+  INTEGER :: nlvl(92, 0:92) !< actual number of levels to consider for ion \f$ j \f$ of
+                            !! atom \f$ i \f$. nlvl\f$ (i,j) = 0 \f$ if \f$ j > i \f$. \f$ n \f$ is equal to the
+                            !! index \f$ k \f$ in the formalism from class. Just for future reference,
+                            !! \f$ i = 1 \rightarrow \f$ hydrogen, \f$ j = 0 \rightarrow \f$ neutral,
+                            !! \f$ k = 1 \rightarrow \f$ ground state.
+                            !! hence the non-standard indices for \f$ j \f$ in a whole bunch of these
                             !! arrays.
   REAL (KIND=dp) :: chilvl(92, 0:92, nlvlmax) !< level energies (ground = 0) (units = eV)
   INTEGER :: g_i(92, 0:92, nlvlmax) !< statistical weight for each level
