d07ff59e1fc89f3c91d987052a95bf99ecddd85a
Brian Friesen <friesen@ou.edu>
2013-04-23 10:23:32 -0500

    Added wavelength column to write_moments routine.

--- src/write_moments.f90
+++ src/write_moments.f90
@@ -11,12 +11,12 @@ subroutine write_moments
   wfmt = '(a15)'
   write( 23, wfmt ) '# NEW ITERATION'
   write( 23, * )
-  wfmt = '(5a12)'
-  write( 23, wfmt ) 'tau', 'J', 'H', 'K', 'B'
-  wfmt = '(5es12.4e2)'
+  wfmt = '(6a12)'
+  write( 23, wfmt ) 'lambda', 'tau', 'J', 'H', 'K', 'B'
+  wfmt = '(6es12.4e2)'
   do i1 = 1, n_wl_pts
     do i2 = 1, n_depth_pts
-      write( 23, wfmt ) tau_grid( i2 ), j_lambda( i2, i1 ), &
+      write( 23, wfmt ) wl_grid(i1), tau_grid( i2 ), j_lambda( i2, i1 ), &
                         h_lambda( i2, i1 ), k_lambda( i2, i1 ), &
                         planck_fn( wl_grid( i1 ) * a2cm, temp )
     end do
